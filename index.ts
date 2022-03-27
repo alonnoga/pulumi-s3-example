6198ce0682378bad8c8181ce91e282d6cb938391
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2022-03-27 16:11:27 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const aws = require("@pulumi/aws");
 const mime = require("mime");
 
 // Create an S3 bucket
-let siteBucket = new aws.s3.Bucket("s3-website-bucket-pulumi");
+let siteBucket = new aws.s3.Bucket("s3-website-bucket-pulumi-eeee");
 
 let siteDir = "www"; // directory for content files
 
